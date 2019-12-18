61266224058bba1af5229faeba98ca3e082f3692
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2019-12-18 13:52:40 -0300

    Added the funcionality of creating the desire .root file if it does not already exists or opening it for updates if it does exist.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
@@ -1,6 +1,21 @@
-#include "TFile.h"
-#include "TH1.h"
+#include <iostream>
 
-void GetChargeDistribution(int run, bool isMC){
-  
+#include <TFile.h>
+#include <TH1.h>
+#include <TSystem.h>
+
+void GetChargeDistribution(int run, bool isMC)
+{
+    // Cheks wether the root file already exists.
+    if (gSystem -> AccessPathName(Form("hist_%d.root", run))){
+      std::cout << "The " << Form("hist_%d.root", run) << " file does not exist. Creating it:" << std::endl;
+      TFile *hist_file = new TFile (Form("hist_%d.root", run), "CREATE");
+
+    } else {
+      std::cout << "Opening the " << Form("hist_%d.root", run) << " file." << std::endl;
+      TFile *hist_file = new TFile (Form("hist_%d.root", run), "UPDATE");
+    }
+
+
+
 }

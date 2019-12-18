a4936fc8291287a23feb2d6ed9d8aa7405548b61
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2019-12-18 14:08:46 -0300

    Added the ability to generate a charge distribution histogram from a given reconstructed data run.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
@@ -4,18 +4,25 @@
 #include <TH1.h>
 #include <TSystem.h>
 
-void GetChargeDistribution(int run, bool isMC)
+TTree *tree_reco;
+
+void GetChargeDistribution(int run, bool isMC = true)
 {
     // Cheks wether the root file already exists.
-    if (gSystem -> AccessPathName(Form("hist_%d.root", run))){
+    /*if (gSystem -> AccessPathName(Form("hist_%d.root", run))){
       std::cout << "The " << Form("hist_%d.root", run) << " file does not exist. Creating it:" << std::endl;
       TFile *hist_file = new TFile (Form("hist_%d.root", run), "CREATE");
 
     } else {
       std::cout << "Opening the " << Form("hist_%d.root", run) << " file." << std::endl;
       TFile *hist_file = new TFile (Form("hist_%d.root", run), "UPDATE");
-    }
+    }*/
 
+    TFile *run_file = new TFile(Form("run_%d%s.root",run, (isMC)?"MC":""));
+    run_file -> GetObject("reco", tree_reco);
+    tree_reco ->SetMarkerStyle(20);
+    tree_reco ->SetMarkerSize(0.5);
 
+    tree_reco -> Draw("clusters[0].charge >> charge_distribution", "clusters[0].f90 > 0 && clusters[0].f90 < 1");
 
 }

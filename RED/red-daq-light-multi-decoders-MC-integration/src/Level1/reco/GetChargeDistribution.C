112693bc7d971b7d34d38309f078a94e926aac5a
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-14 11:32:48 -0300

    Added a line to state the macro has finished running.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
@@ -42,7 +42,9 @@ void GetChargeDistribution(int run, bool isMC = false, int ERorNR = 0)
 
   charge_dist -> Write(Form("charge_distribution%s%s%s", (isMC)?"MC":"", (ERorNR == 1)?"ER":"", (ERorNR == 2)?"NR":""), TObject::kOverwrite);
   hist_file -> Close();
-  delete charge_dist;
 
+  std::cout << "Charge histogram generated." << std::endl;
+
+  delete charge_dist;
   gROOT->SetBatch(kFALSE);
 }

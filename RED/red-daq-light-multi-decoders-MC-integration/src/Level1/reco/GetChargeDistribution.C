7c143ea8c9a28e6094a74586da018d33ec948108
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-14 10:01:00 -0300

    Completed the code.
    Now just need to test run and debug.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/GetChargeDistribution.C
@@ -2,41 +2,40 @@
 
 #include <TFile.h>
 #include <TH1.h>
+#include <TObject.h>
+#include <TString.h>
 #include <TSystem.h>
+#include <TTree.h>
 
-TTree *tree_reco;
 
 void GetChargeDistribution(int run, bool isMC = false, int ERorNR = 0)
 {
+
+    TFile *run_file = new TFile(Form("run_%d%s%s%s.root",run, (isMC)?"MC":"", (ERorNR == 1)?"ER":"", (ERorNR == 2)?"NR":""));
+    TTree *tree_reco;
+
+    run_file  -> GetObject("reco", tree_reco);
+    tree_reco -> SetMarkerStyle(20);
+    tree_reco -> SetMarkerSize(0.5);
+    tree_reco -> Draw("clusters[0].charge >> charge_dist", "clusters[0].f90 > 0 && clusters[0].f90 < 1");
+
+    TH1F *charge_dist = (TH1F*)gDirectory -> Get("charge_dist");
+    charge_dist -> SetName(Form("charge_distribution%s%s%s", (isMC)?"MC":"", (ERorNR == 1)?"ER":"", (ERorNR == 2)?"NR":""));
+    charge_dist -> SetTitle("Charge Distribution; Charge (in PE)");
+
+    /*run_file -> Close();
+
     // Cheks wether the root file already exists.
     if (gSystem -> AccessPathName(Form("hist_%d.root", run))){
       std::cout << "The " << Form("hist_%d.root", run) << " file does not exist. Creating it..." << std::endl;
-      TFile *hist_file = new TFile (Form("hist_%d.root", run), "CREATE");
+      TFile *hist_file = new TFile(Form("hist_%d.root", run), "CREATE");
 
     } else {
         std::cout << "Opening the " << Form("hist_%d.root", run) << " file." << std::endl;
-        TFile *hist_file = new TFile (Form("hist_%d.root", run), "UPDATE");
+        TFile *hist_file = new TFile(Form("hist_%d.root", run), "UPDATE");
 
         if ( !(hist_file -> IsOpen()) ) { std::cout << "Unable to open file." << std::endl;}
     }
 
-    // Might need to change the placemente of the code abover. Or just find a way to change the currenct directory, so that the histogram is saved in the
-    // correct file ("hist_%d.root").
-
-    TFile *run_file = new TFile(Form("run_%d%s.root",run, (isMC)?"MC":""));
-    run_file -> GetObject("reco", tree_reco);
-    tree_reco ->SetMarkerStyle(20);
-    tree_reco ->SetMarkerSize(0.5);
-
-    tree_reco -> Draw("clusters[0].charge >> charge_distribution", "clusters[0].f90 > 0 && clusters[0].f90 < 1");
-
-    TH1F *charge_dist = (TH1F*)gDirectory -> Get("charge_distribution");
-    charge_dist -> SetName(Form("charge_distribution%s%s%s", (isMC)?"MC":"", (ERorNR == 1)?"ER":"", (ERorNR == 2)?"NR":""));
-    charge_dist -> SetTitle("Charge Distribution; Charge (in PE)");
-
-
-
-    // Might add some way to rename the created histogram depending on the original root file. Maybe adding the possibility of adding MC, ER and NR to the end.
-    // Maybe just add the possibility of the user himself changing the name? It opens the possibility of less cosistency though.
-    // Now must save the created histogram
+    charge_dist -> Write(Form("charge_distribution%s%s%s", (isMC)?"MC":"", (ERorNR == 1)?"ER":"", (ERorNR == 2)?"NR":""));*/
 }

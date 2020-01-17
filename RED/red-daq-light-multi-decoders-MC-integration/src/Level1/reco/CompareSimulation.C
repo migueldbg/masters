8603adf5d44a50387179f36a8818a23d1cda51aa
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-17 12:14:18 -0300

    Began attempts to create a function that creates the f90 histograms.
    I felt like it was necessary so I wouldn't be stuck writing virtually the same line of code three times for each data set.;

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
@@ -8,20 +8,42 @@
 #include <TTree.h>
 #include <ROOT/RDataFrame.hxx>
 
-void CompareSimulation(int run, int number_divisions = 10, double max_charge = 2000){
 
-  TFile *run_file = new TFile(Form("run_%d.root", run));
-  //TFile *MCER_file = new TFile(Form("run_%dMCER.root", run));
-  //TFile *MCNR_file = new TFile(Form("run_%dMCNR.root", run));
+TH1F* MyMacro(int count) {
+  auto tmp = new TH1F("tmp_hist", "Test Histogram", count, 0, count);
 
-  TTree *tree_reco_run;
-  TTree *tree_reco_MCER;
-  TTree *tree_reco_MCNR;
+  for(int i = 0; i < count; i++) {
+    tmp->Fill(gRandom->Rndm(1)*count);
+  }
 
-  run_file  -> GetObject("reco", reco_run);
-  MCER_file -> GetObject("reco", reco_MCER);
-  MCNR_file -> GetObject("reco", reco_MCNR);
-
-  
-
+  return(tmp);
 }
+
+void CompareSimulation(int run, int number_divisions = 10, double max_charge_run = 2000){
+
+  //double max_charge_MCER = 20000;
+  //double max_charge_MCNR = 9000;
+  TH1F* hist = MyMacro(10000);
+  hist -> Draw();
+
+
+  /*
+  TFile *run_file = new TFile(Form("run_%d.root", run));
+  TFile *MCER_file = new TFile(Form("run_%dMCER.root", run));
+  TFile *MCNR_file = new TFile(Form("run_%dMCNR.root", run));
+
+  TTree *reco_run;    run_file  -> GetObject("reco", reco_run);
+  TTree *reco_MCER;   MCER_file -> GetObject("reco", reco_MCER);
+  TTree *reco_MCNR;   MCNR_file -> GetObject("reco", reco_MCNR);
+
+  double division_size_run  = max_charge_run/number_divisions;
+  double division_size_MCER = max_charge_MCER/number_divisions;
+  double division_size_MCNR = max_charge_MCNR/number_divisions;
+  */
+}
+
+
+/*TH1* Crea(){
+  TFile *run_data = new TFile(file_name);
+
+}*/

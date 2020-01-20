1a23f09fd87ac9f38872486169de6e70937d0e16
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-20 16:24:06 -0300

    Updated "Createf90DIst" function.
    Made it so that the histogram generates errors, along with normalizing it for better comparison.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
@@ -14,13 +14,19 @@ TH1F* Createf90Dist(char *file_name, double *charge_max, double *f90_min, double
   TFile *run_data = new TFile(file_name);
   TTree *reco_data; run_data -> GetObject("reco", reco_data);
 
-  double cbin_up  = bin_number * division_size
-  double cbin_low = (bin_number - 1) * division_size;
+  Double_T cbin_up  = bin_number * division_size
+  Double_T cbin_low = (bin_number - 1) * division_size;
 
   reco_data -> Draw("clusters[0].f90 >> hist",
                Form("clusters[0].f90 >= %d && clusters[0].f90 <= %d && clusters[0].charge >= %d && clusters[0].charge <= %d", f90_min, f90_max, cbin_low, cbin_up),
                "goff");
   TH1F* f90_dist = (TH1F*)gDirectory -> Get("hist")
+
+  if (f90_dist -> GetSumw2N() == 0) f90_dist -> Sumw2(kTRUE);
+
+  Double_t norm;
+  Double_t scale = norm/(f90_dist -> Integral());
+  f90_dist -> Scale(scale);
 
   return f90_dist;
 

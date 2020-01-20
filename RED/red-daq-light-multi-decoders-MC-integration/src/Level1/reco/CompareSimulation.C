5136ac059d804ea26e1728f64c14b6f6e0045483
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-20 16:49:55 -0300

    Testing the functionality of "Creatf90Dist"
    It seems to create the desired histogram, however when the histogram is normalized, nothing is drawn. But it seems that something is working even in this case, since it has the avg. and std. dev. still

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
@@ -10,38 +10,40 @@
 #include <ROOT/RDataFrame.hxx>
 
 
-TH1F* Createf90Dist(char *file_name, double *charge_max, double *f90_min, double *f90_max, double *division_size; int *bin_number) {
+TH1F* Createf90Dist(char *file_name, Double_t charge_max, Double_t f90_min, Double_t f90_max, Double_t division_size, int bin_number) {
   TFile *run_data = new TFile(file_name);
   TTree *reco_data; run_data -> GetObject("reco", reco_data);
 
-  Double_T cbin_up  = bin_number * division_size
-  Double_T cbin_low = (bin_number - 1) * division_size;
+  Double_t cbin_up  = bin_number * division_size;
+  Double_t cbin_low = (bin_number - 1) * division_size;
 
   reco_data -> Draw("clusters[0].f90 >> hist",
-               Form("clusters[0].f90 >= %d && clusters[0].f90 <= %d && clusters[0].charge >= %d && clusters[0].charge <= %d", f90_min, f90_max, cbin_low, cbin_up),
-               "goff");
-  TH1F* f90_dist = (TH1F*)gDirectory -> Get("hist")
+               Form("clusters[0].f90 >= %f && clusters[0].f90 <= %f && clusters[0].charge >= %f && clusters[0].charge <= %f", f90_min, f90_max, cbin_low, cbin_up),"goff");
+  TH1F* f90_dist = (TH1F*)gDirectory -> Get("hist");
 
   if (f90_dist -> GetSumw2N() == 0) f90_dist -> Sumw2(kTRUE);
 
-  Double_t norm;
+  /*Double_t norm;
   Double_t scale = norm/(f90_dist -> Integral());
   f90_dist -> Scale(scale);
-
+  */
   return f90_dist;
 
   /*for(int i = 0; i < count; i++) {
     tmp->Fill(gRandom->Rndm(1)*count);
   }
 
-  return((TH1F*)tmp->Clone());
-}*/
+  return((TH1F*)tmp->Clone());*/
+}
 
 void CompareSimulation(int run, int number_divisions = 10, double max_charge_run = 2000){
 
   //double max_charge_MCER = 20000;
   //double max_charge_MCNR = 9000;
 
+
+  TH1F* dist = Createf90Dist(Form("run_1220.root"), max_charge_run, 0., 1., 200., 4);
+  dist -> Draw();
 
   /*
   TFile *run_file = new TFile(Form("run_%d.root", run));

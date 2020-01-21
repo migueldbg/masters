a01dc69f0f191721262969a27d9d659d633463ba
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-21 13:10:18 -0300

    Began to apply the "Createf90Dist" to generate the desired histograms.
    Chose to use arrays to facilitate future code changes. For now, I've just prototyped the array changes with generating a single histogram, with success. Now must begin testing to see if all histograms are at least generated.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <array>
+#include <string>
 
 //#include "../../Modules/RDCluster.hh"
 #include <TFile.h>
@@ -32,26 +34,32 @@ TH1F* Createf90Dist(char *file_name, Double_t charge_max, Double_t f90_min, Doub
   return f90_dist;
 }
 
-void CompareSimulation(int run, int number_divisions = 10, double max_charge_run = 2000){
+void CompareSimulation(int run, int number_divisions = 10, Double_t max_charge_run = 2000., Double_t max_charge_MCER = 20000.0, Double_t max_charge_MCNR = 9000.0){
 
-  //double max_charge_MCER = 20000;
-  //double max_charge_MCNR = 9000;
+  std::array<Double_t, 3> max_charge = {max_charge_run, max_charge_MCER, max_charge_MCNR};
+
+  TH1F** f90hist_run  = new TH1F*[number_divisions];
+  TH1F** f90hist_MCER = new TH1F*[number_divisions];
+  TH1F** f90hist_MCNR = new TH1F*[number_divisions];
+
+  string file_suffix[3] = {"", "MCER", "MCNR"};
+
+  /*for (int i = 0; i < file_suffix.size(); i++){
+
+    for (int j = 0; j < number_divisions; j++){
+      if (i == 0){
+        f90hist_run[j] = Createf90Dist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], 0., 1., number_divisions, j+1);
+      } else if (i == 1) {
+        f90hist_MCER[j] = Createf90Dist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], 0., 1., number_divisions, j+1);
+      } else if (i == 2) {
+        f90hist_MCER[j] = Createf90Dist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], 0., 1., number_divisions, j+1);
+      }
+    }
+
+  }*/
 
 
-  TH1F* dist = Createf90Dist(Form("run_1220.root"), max_charge_run, 0., 1., 200., 4);
+  TH1F* dist = Createf90Dist(Form("run_%d%s.root", run, file_suffix[0].c_str()), max_charge[0], 0., 1., number_divisions, 4);
   dist -> Draw();
 
-  /*
-  TFile *run_file = new TFile(Form("run_%d.root", run));
-  TFile *MCER_file = new TFile(Form("run_%dMCER.root", run));
-  TFile *MCNR_file = new TFile(Form("run_%dMCNR.root", run));
-
-  TTree *reco_run;    run_file  -> GetObject("reco", reco_run);
-  TTree *reco_MCER;   MCER_file -> GetObject("reco", reco_MCER);
-  TTree *reco_MCNR;   MCNR_file -> GetObject("reco", reco_MCNR);
-
-  double division_size_run  = max_charge_run/number_divisions;
-  double division_size_MCER = max_charge_MCER/number_divisions;
-  double division_size_MCNR = max_charge_MCNR/number_divisions;
-  */
 }

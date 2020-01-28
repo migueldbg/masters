81baa89df34453a3f5982132aa4ce6fe24d0794c
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-01-28 17:02:37 -0300

    Currently attempting to write a simple function
    The function takes the name of a directory as a string and check if it exists or not. If it does, it returns it. If it doesn't, it creates it and then returns it.

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CompareSimulation.C
@@ -38,18 +38,29 @@ TH1F* Generatef90Hist(char *file_name, Double_t charge_max, Double_t f90_min, Do
   return f90_dist;
 }
 
+TDirectory* MakeDir(string dir_name, string dir_title){
+  const char *dir1 = dir_name.c_str();
+  const char *dir2 = dir_title.c_str();
+  TDirectory *dir = gDirectory -> GetDirectory(dir1);
+  if (!dir) dir = gDirectory -> mkdir(dir1, dir2);
+
+  return dir;
+}
+
+
 int GenerateAllHist(int run, int number_divisions, Double_t f90_min, Double_t f90_mid, Double_t f90_max,
                        Double_t max_charge_run, Double_t max_charge_MCER, Double_t max_charge_MCNR){
 
   std::array<Double_t, 3>    max_charge  = {max_charge_run, max_charge_MCER, max_charge_MCNR};
   std::array<std::string, 3> file_suffix = {"", "MCER", "MCNR"};
+  std::array<std::string, 1> dir_name = {"data"};
 
   TH1F** f90hist_run   = new TH1F*[number_divisions];
-  TH1F** f90hist_runER = new TH1F*[number_divisions];
-  TH1F** f90hist_runNR = new TH1F*[number_divisions];
+  //TH1F** f90hist_runER = new TH1F*[number_divisions];
+  //TH1F** f90hist_runNR = new TH1F*[number_divisions];
   TH1F** f90hist_MCER  = new TH1F*[number_divisions];
   TH1F** f90hist_MCNR  = new TH1F*[number_divisions];
-  THStack** f90hstack  = new THStack*[number_divisions];
+  //THStack** f90hstack  = new THStack*[number_divisions];
 
   // Cheks wether the root file already exists and tells the user. The "UPDATE" option already takes into account the possibility of the file not existing.
   if (gSystem -> AccessPathName(Form("hist_%d.root", run))){
@@ -58,7 +69,7 @@ int GenerateAllHist(int run, int number_divisions, Double_t f90_min, Double_t f9
     std::cout << "Opening file: " << Form("hist_%d.root", run) << std::endl;
   }
 
-  TFile *hist_file = new TFile(Form("test_%d.root", run), "UPDATE");
+  TFile *hist_file = new TFile(Form("hist_%d.root", run), "UPDATE");
   if ( !(hist_file -> IsOpen()) ) { std::cout << "Unable to open file." << std::endl;}
 
   // Checks wether a folder for saving the f90 histograms exists. If not, create it.
@@ -66,10 +77,18 @@ int GenerateAllHist(int run, int number_divisions, Double_t f90_min, Double_t f9
   if (!f90_dir) f90_dir = hist_file -> mkdir("f90_histograms", "f90_hist");
 
   f90_dir -> cd();
-  TDirectory *data_dir = f90_dir -> GetDirectory("data");
-  if (!data_dir) data_dir = f90_dir -> mkdir("data", "data");
+  TDirectory *data_dir = MakeDir(dir_name[0], dir_name[0]);
+  //TDirectory *data_dir = f90_dir -> GetDirectory("data");
+  //if (!data_dir) data_dir = f90_dir -> mkdir("data", "data");
+  //data_dir -> cd();
+  //TDirectory *data_dir_both = data_dir -> GetDirectory("both");
+  //if (!data_dir_both) data_dir_both = data_dir -> mkdir("both", "both");
+  //TDirectory *data_dir_ER = data_dir -> GetDirectory("ER");
+  //if (!data_dir_ER)
   TDirectory *MC_dir = f90_dir -> GetDirectory("monte_carlo");
   if (!MC_dir) MC_dir = f90_dir -> mkdir("monte_carlo", "monte_carlo");
+
+
 
   // Generate all the desire f90 histograms (run, run er, run nr, er only and nr only).
   for (int i = 0; i < 3; i++){
@@ -81,7 +100,9 @@ int GenerateAllHist(int run, int number_divisions, Double_t f90_min, Double_t f9
 
         data_dir -> WriteObject(f90hist_run[j], Form("f90_distribution%d", j+1), "OverWrite");
 
-        f90hist_runER[j] = Generatef90Hist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], f90_min, f90_mid, number_divisions, j+1);
+        //f90hist_run[j] -> Write(Form("f90_distribution%d", j+1), TObject::kOverwrite);
+
+        /*f90hist_runER[j] = Generatef90Hist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], f90_min, f90_mid, number_divisions, j+1);
         f90hist_runER[j] -> SetName(Form("f90_distribution_ER%d", j+1));
         f90hist_runER[j] -> SetTitle(Form("f90 Distribution (ER, Bin Number: %d); f09", j+1));
 
@@ -91,44 +112,36 @@ int GenerateAllHist(int run, int number_divisions, Double_t f90_min, Double_t f9
         f90hist_runNR[j] -> SetName(Form("f90_distribution_NR%d", j+1));
         f90hist_runNR[j] -> SetTitle(Form("f90 Distribution (NR, Bin Number: %d); f09", j+1));
 
-        data_dir -> WriteObject(f90hist_runNR[j], Form("f90_distribution_NR%d", j+1), "OverWrite");
+        data_dir -> WriteObject(f90hist_runNR[j], Form("f90_distribution_NR%d", j+1), "OverWrite");*/
 
-      } else if (i == 1) {
+      }/* else if (i == 1) {
         f90hist_MCER[j] = Generatef90Hist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], f90_min, f90_mid, number_divisions, j+1);
         f90hist_MCER[j] -> SetName(Form("f90_distribution_%s%d", file_suffix[i].c_str(), j+1));
         f90hist_MCER[j] -> SetTitle(Form("f90 Distrbution (MC, Bin Number: %d); f90", j+1));
 
-        MC_dir -> WriteObject(f90hist_MCER[j], Form("f90_distribution_%s%d", file_suffix[i].c_str(), j+1), "OverWrite");
+        MC_dir -> cd();
+        f90hist_MCER[j] -> Write(Form("f90_distribution_%s%d", file_suffix[i].c_str(), j+1), TObject::kOverwrite);
       } else if (i == 2) {
         f90hist_MCNR[j] = Generatef90Hist(Form("run_%d%s.root", run, file_suffix[i].c_str()), max_charge[i], f90_mid, f90_max, number_divisions, j+1);
         f90hist_MCNR[j] -> SetName(Form("f90_distribution_%s%d", file_suffix[i].c_str(), j+1));
         f90hist_MCNR[j] -> SetTitle(Form("f90 Distrbution (MC, Bin Number: %d); f90", j+1));
 
-        MC_dir -> WriteObject(f90hist_MCNR[j], Form("f90_distribution_%s%d", file_suffix[i].c_str(), j+1), "OverWrite");
-      }
+        MC_dir -> cd();
+        f90hist_MCNR[j] -> Write(Form("f90_distribution_%s%d", file_suffix[i].c_str(), j+1), TObject::kOverwrite);
+      }*/
     }
   }
 
   // Create histograms with all three distributions for each division (visualization).
-  for (int j = 0; j < number_divisions; j++){
+  /*for (int j = 0; j < number_divisions; j++){
     f90hstack[j] -> Add(f90hist_run[j]); f90hstack[j] -> Add(f90hist_MCER[j]); f90hstack[j] -> Add(f90hist_MCNR[j]);
     f90hstack[j] -> SetName(Form("f90_distribution_all%d", j+1));
     f90hstack[j] -> SetTitle(Form("f90 Distribution (All, Bin Number: %d); f90",  j+1));
 
     f90_dir -> WriteObject(f90hstack[j],  Form("f90_distribution_all%d", j+1), "OverWrite");
   }
-
+  */
   hist_file -> Close();
-
-  // I'm not sure if this final part is necessary, but better safe then sorry
-  delete hist_file; delete f90_dir; delete data_dir; delete MC_dir;
-
-  delete[] f90hist_run;
-  delete[] f90hist_runER;
-  delete[] f90hist_runNR;
-  delete[] f90hist_MCER;
-  delete[] f90hist_MCNR;
-  delete[] f90hstack;
 
   return 0;
 }

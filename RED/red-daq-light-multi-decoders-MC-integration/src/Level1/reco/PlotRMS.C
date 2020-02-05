d3b0f9b63d57f33a3460e789bad4074dadf84db5
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-02-05 11:03:56 -0300

    Created the 'PlotRMS.C' file.
    This file will contain the macro to plot the RMS of the f90 histograms generated by 'CreateF90Histograms.C' as a function of total charge.

---
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/PlotRMS.C
@@ -0,0 +1,25 @@
+/* File: PlotRMS.C (ROOT macro).
+ *
+ * Author: Miguel Del Ben Galdiano (miguel.galdiano@gmail.com).
+ * Date of creation : February 5 2020.
+ * Last Update      : February 5 2020.
+ *
+ * Summary of File:
+ *
+ *    stuff
+ */
+
+#include <array>
+
+#include <TFile.h>
+#include <TH1.h>
+#include <TObject.h>
+#include <TString.h>
+#include <TSystem.h>
+#include <TTree.h>
+
+void PlotRMS(int run){
+
+
+
+}

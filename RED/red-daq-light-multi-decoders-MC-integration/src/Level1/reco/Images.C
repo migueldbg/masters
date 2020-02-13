0015bb00447f45a0027984b783b891790d86ed65
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-02-13 16:15:40 -0300

    Finished the code to generate the desired images

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/Images.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/Images.C
@@ -3,7 +3,6 @@
 #include <TString.h>
 #include <TTree.h>
 
-
 TH2F* F90vChargeHistogram( TString file_name ){
 
   TFile* file = new TFile( file_name );
@@ -18,7 +17,35 @@ TH2F* F90vChargeHistogram( TString file_name ){
   return f90vCharge;
 }
 
-void F90vChargeCanvas( int run ){
+TH1F* F90ClusterHist( TString file_name, int cluster ){
+
+  TFile* file = new TFile( file_name );
+  TTree* reco; file -> GetObject("reco", reco);
+
+  reco -> Draw( Form("clusters[%d].f90 >> hist", cluster), Form("clusters[%d].f90 > 0. && clusters[%d].f90 < 1.", cluster, cluster), "goff" );
+  TH1F* f90cluster_hist = (TH1F*) gDirectory -> Get("hist");
+
+  f90cluster_hist -> SetDirectory(0);
+  file -> Close();
+
+  return f90cluster_hist;
+}
+
+TH1F* F90SIPMHist( TString file_name, int sipm_number ){
+
+  TFile* file = new TFile( file_name );
+  TTree* reco; file -> GetObject("reco", reco);
+
+  reco -> Draw( Form("f90[%d] >> hist", sipm_number), Form("f90[%d] > 0. && f90[%d] < 1.", sipm_number, sipm_number), "goff" );
+  TH1F* f90sipm_hist = (TH1F*) gDirectory -> Get("hist");
+
+  f90sipm_hist -> SetDirectory(0);
+  file -> Close();
+
+  return f90sipm_hist;
+}
+
+void GenerateF90vChargeCanvas( int run ){
 
   TH2F* f90vCharge_data  = F90vChargeHistogram( Form("run_%d.root",      run) );  f90vCharge_data  -> SetTitle("f90 vs Charge (1220); Charge (PE); f90");
   TH2F* f90vCharge_mc_er = F90vChargeHistogram( Form("run_%d_MCER.root", run) );  f90vCharge_mc_er -> SetTitle("f90 vs Charge (1220, MC ER); Charge (PE); f90");
@@ -31,8 +58,6 @@ void F90vChargeCanvas( int run ){
   TLine* f90_low = new TLine(0., 0.2,  1050., 0.2);    f90_low -> SetLineStyle(10);  f90_low -> SetLineWidth(3); f90_low -> SetLineColor(13);
   TLine* f90_mid = new TLine(0., 0.4,  1050., 0.4);    f90_mid -> SetLineStyle(10);  f90_mid -> SetLineWidth(3); f90_mid -> SetLineColor(13);
   TLine* f90_upp = new TLine(0., 0.65, 1050., 0.65);   f90_upp -> SetLineStyle(10);  f90_upp -> SetLineWidth(3); f90_upp -> SetLineColor(13);
-
-  gROOT->SetBatch(kTRUE);
 
   Double_t y_size = 1000.;
   Double_t x_size = 2 * 1.61803398875 * y_size;
@@ -47,13 +72,51 @@ void F90vChargeCanvas( int run ){
   f90vCharge_mc_nr -> Draw(); f90vCharge_mc_er -> Draw("same"); f90_low -> Draw("same"); f90_mid -> Draw("same"); f90_upp -> Draw("same");
 
   f90vC_canvas -> SaveAs("f90 v Charge Distribution.png"); f90vC_canvas -> SaveAs("f90 v Charge Distribution.pdf");
+}
 
-  gROOT->SetBatch(kFALSE);
+void GenerateF90comparisonCanvas( int run, int cluster, int number_of_sipm, bool isMC = false){
+
+  TH1F* f90cluster_hist = F90ClusterHist(Form("run_%d%s.root", run, isMC ? "_MCER":""), cluster);  f90cluster_hist -> SetTitle("f90 Distribution (Cluster); f90");
+
+  TH1F** f90sipm_hist = new TH1F*[number_of_sipm];
+  for (Int_t i = 0; i < number_of_sipm; i++){
+    f90sipm_hist[i] = F90SIPMHist( Form("run_%d%s.root", run, isMC ? "_MCER":""), i );
+  }
+
+  Double_t y_size = 1000.;
+  Double_t x_size = 2 * 1.61803398875 * y_size;
+
+  TCanvas* f90comparison_canvas = new TCanvas("f90comparison_canvas", "f90comparison_canvas", x_size, y_size);
+  f90comparison_canvas -> Divide(2,1);
+
+  f90comparison_canvas -> cd(1);  f90cluster_hist -> Draw("hist");
+
+  f90comparison_canvas -> cd(2);
+  for (int i = 0; i < number_of_sipm; i++){
+    f90sipm_hist[0] -> SetTitle("f90 Distribution (SIPMs); f90");
+    f90sipm_hist[i] -> Draw(i == 0 ? "hist":"same");
+  }
+
+  if ( isMC ){
+    f90comparison_canvas -> SaveAs("f90 Distribution Comparison (1220, MC).png");
+    f90comparison_canvas -> SaveAs("f90 Distribution Comparison (1220, MC).pdf");
+  } else {
+    f90comparison_canvas -> SaveAs("f90 Distribution Comparison (1220, Data).png");
+    f90comparison_canvas -> SaveAs("f90 Distribution Comparison (1220, Data).pdf");
+  }
+
 }
 
 void Images( int run ){
 
-  F90vChargeCanvas( run );
+  int cluster = 0;
+  int number_of_sipm = 28;
+  bool isMC = true;
 
+  GenerateF90vChargeCanvas( run );
+
+  GenerateF90comparisonCanvas( run, cluster, number_of_sipm );
+
+  GenerateF90comparisonCanvas( run, cluster, number_of_sipm, isMC );
 
 }

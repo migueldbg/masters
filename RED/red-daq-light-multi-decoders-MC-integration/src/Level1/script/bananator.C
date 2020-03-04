26b6b8de4820ef05368e9962035d2ec5d8f5375d
Miguel Del Ben Galdiano <miguel.galdiano@gmail.com>
2020-03-04 10:02:29 -0300

    Auto stash before checking out "HEAD"

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/script/bananator.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/script/bananator.C
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
-#include <algorithm> 
-#include <vector> 
-#include <sstream> 
+#include <algorithm>
+#include <vector>
+#include <sstream>
 
 #include <TFile.h>
 #include <TF1.h>
@@ -21,9 +21,9 @@
 #include <TAxis.h>
 void bananator(int run, int nbin=100, int xmin=0, int xmax=20000, int ymin=0, int ymax=20000){
 //run numbers, bin number, xmin, xmax, ymin, ymax
-    
+
     TFile *f = new TFile(Form("../run_%d.root", run), "read");
-   
+
 
     TTree *data = (TTree*)f->Get("reco");
 
@@ -31,20 +31,20 @@ void bananator(int run, int nbin=100, int xmin=0, int xmax=20000, int ymin=0, in
     gROOT->SetStyle("Plain");
     gStyle->SetOptStat(2210);
     gStyle->SetOptFit();
-    
-    
+
+
 ////////////// Bananator ////////////////////
 
     TCanvas *c1 = new TCanvas("c1","c1",10,10,900,600);
-    
+
     c1->cd();
     c1->SetLogz();
-    
+
     TString title = "#DeltaE / E Spectrum ";
-    
+
     TH2F *h = new TH2F("h",title,nbin,xmin,xmax,nbin,ymin,ymax);
-    
-    data->Draw("ymin[30]-baseline_mean[30]:ymin[31]-baseline_mean[31]>>h");
+
+    data->Draw("-ymin[30]+baseline_mean[30]:-ymin[31]baseline_mean[31]>>h");
 
     h->GetXaxis()->SetTitle("E [ADC counts]");
     h->GetXaxis()->SetTitleOffset(1.16);
@@ -53,5 +53,5 @@ void bananator(int run, int nbin=100, int xmin=0, int xmax=20000, int ymin=0, in
 
     h->DrawCopy("colz");
 
-    
+
 }

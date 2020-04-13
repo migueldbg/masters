7b2f63be3aefc6d3b71799c4de875d1c1926bef0
Miguel <miguel.galdiano@gmail.com>
2020-04-12 22:35:01 -0300

    Changed file name: ToFStudy.C -> F90vsToF.C

---
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/F90vsToF.C
@@ -0,0 +1,12 @@
+#include <TCut.h>
+#include <TFile.h>
+#include <TH1.h>
+#include <TObject.h>
+#include <TString.h>
+#include <TSystem.h>
+#include <TTree.h>
+
+void study(int run, Double_t charge_min = 20, Double_t charge_max = 1000){
+
+  
+}

91d8d55284073bff943aedca87abebccfc7402a1
Miguel <miguel.galdiano@gmail.com>
2020-04-28 18:14:39 -0300

    Added low Berilium cut,  ToF cut and modified CreateF90Histograms
    Low Be Cut: this was implemented by defining a rectangle surrounding the low energy Berilium blob.  I'm currently trying to implement the use of a TCutG to more precisely select the low Be blob. As such, this will probably be changed.
    ToF Cut: based on a quick gaussian fit to the ToF histogram obtained from the 1501 to 1521 runs. The current definition of ToF is under review, as such this will also probably change.
    CreateF90Histograms (): changed the function to properly implement the CreateERF90Histograms and CreateNRF90Histograms functions

--- RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CreateF90Histograms-ToFver.C
+++ RED/red-daq-light-multi-decoders-MC-integration/src/Level1/reco/CreateF90Histograms-ToFver.C
@@ -174,94 +174,70 @@ void CreateF90NRHistograms ( int run, Int_t exp_cfg, bool isMC = false, bool tof
   else if ( isMC ) { run_file_name = Form("runs/run_%d_MCNR.root", run); }
 
   // NUCLEAR RECOIL EVENT SELECTION PARAMETERS //
-  Double_t f90_min; Double_t f90_max;
-  Double_t tof_min; Double_t tof_max; // If the ToF border values are equal, the code doesn't apply a ToF cut.
-  // ----------------------------------------- //
+  Double_t f90_min;  Double_t f90_max;
+  Double_t tof_min;  Double_t tof_max; // If the ToF border values are equal, the code doesn't apply a ToF cut.
 
   if ( tof_cut ){
     f90_min = 0.0;  f90_max = 1.0;
-    tof_min = 0.0;  tof_max = 50;
+    tof_min = 30;  tof_max = 42;  // based on runs 1501 to 1521.
   } else if ( !tof_cut ) {
     f90_min = 0.4;  f90_max = 0.7;
     tof_min = 0.0;  tof_max = 0.0;
   }
+  // ----------------------------------------- //
+
+  // LOW ENERGY BERILIUM BLOB SELECTION (basend on runs 1501 to 1521) //
+  Double_t E_min  = 2250.; Double_t E_max  = 2725.;
+  Double_t dE_min = 700.;  Double_t dE_max = 850.;
+
+  TCut E_min_cut  = Form("baseline_mean[31] - ymin[31] > %f", E_min);
+  TCut E_max_cut  = Form("baseline_mean[31] - ymin[31] < %f", E_max);
+  TCut dE_min_cut = Form("baseline_mean[30] - ymin[30] > %f", dE_min);
+  TCut dE_max_cut = Form("baseline_mean[30] - ymin[30] < %f", dE_max);
+
+  TCut lowBe_cut = E_min_cut && E_max_cut && dE_min_cut && dE_max_cut;
+  // ---------------------------------------------------------------- //
 
   Double_t bin_size = ( max_charge - min_charge ) / number_of_bins;
-  Double_t charge_low;   Double_t charge_up;
-  TString hist_name;     TString hist_title;
-  TCut histogram_cuts;
+  Double_t s1_charge_low;  Double_t s1_charge_up;
 
-  for (int i = 0; i < number_of_bins; i++){
+  TString  hist_name;      TString  hist_title;
+  TCut histogram_cuts;  TCut total_cuts;
+
+  for (Int_t i = 0; i < number_of_bins; i++){
 
     // Calculate the boundaries of the current bin.
-    charge_low = ( i       * bin_size ) + min_charge;
-    charge_up  = ( (i + 1) * bin_size ) + min_charge;
+    s1_charge_low = ( i       * bin_size ) + min_charge;
+    s1_charge_up  = ( (i + 1) * bin_size ) + min_charge;
 
-    histogram_cuts = DefineCuts(exp_cfg, f90_min, f90_max, charge_low, charge_up, tof_min, tof_max);
+    histogram_cuts = DefineCuts(exp_cfg, f90_min, f90_max, s1_charge_low, s1_charge_up, 0, 50000, tof_min, tof_max);
+    total_cuts = histogram_cuts && lowBe_cut;
 
     hist_name  = Form( "f90_histogram_%snr_%d", (isMC)?"mc":"", i+1 );
-    hist_title = Form( "f90 Distribution (%s, Charge Interval: %d - %d PE); f90", (isMC)?"MC NR":"NR", (int) charge_low, (int) charge_up );
+    hist_title = Form( "f90 Distribution (%s, Charge Interval: %d - %d PE); f90", (isMC)?"MC NR":"NR", (int) s1_charge_low, (int) s1_charge_up );
 
-    WriteF90Hist( dir, run_file_name, histogram_cuts, hist_name, hist_title );
+    WriteF90Histogram( dir, run_file_name, total_cuts, 100, f90_min, f90_max, false, hist_name, hist_title);
   }
 
   output_file -> Close();
 }
 
-void CreateF90Histograms (int run, Double_t bin_size = 20., Double_t max_charge = 1000., Double_t min_charge = 20.){
 
-  TString file_name = Form("analysis_%d.root", run);
-  TFile* hist_file = CheckFile(file_name);
 
-  // --------------------- CREATING NECESSARY DIRECTORIES ---------------------- //
-  hist_file -> cd();
-  TDirectory* histograms_dir     = MakeDirectory("histograms", "histograms");
+void CreateF90Histograms ( int run, Int_t exp_cfg, bool isMC = false, bool tof_cut = false, Int_t ERorNR = 10, Int_t number_of_bins = 50, Double_t min_charge = 20., Double_t max_charge = 1000. ){
 
-  histograms_dir -> cd();
-  TDirectory* f90_histograms_dir = MakeDirectory("f90", "f90");
-
-  f90_histograms_dir -> cd();
-  TDirectory* data_dir           = MakeDirectory("data", "data");
-  TDirectory* monte_carlo_dir    = MakeDirectory("monte_carlo", "monte_carlo");
-
-  data_dir -> cd();
-  TDirectory* da_both_dir         = MakeDirectory("both","both");
-  TDirectory* da_er_dir           = MakeDirectory("ER","ER");
-  TDirectory* da_nr_dir           = MakeDirectory("NR","NR");
-
-  monte_carlo_dir -> cd();
-  TDirectory* mc_er_dir           = MakeDirectory("ER","ER");
-  TDirectory* mc_nr_dir           = MakeDirectory("NR","NR");
-  // --------------------------------------------------------------------------- //
-
-  // These values define the ranges used to obtain the total f90 distribution, the ER only peak and the NR only peak.
-  // Total -> f90_min - f90_max; ER only -> f90_min - f90_mid; NR only -> f90_mid - f90_max.
-  Double_t f90_min = 0.2;
-  Double_t f90_mid = 0.4;
-  Double_t f90_max = 0.6;
-
-  // These variables are used to determine the maximum number of bins, as well as saving the charge boundaries of a given bin.
-  int number_of_divisions = (int) (max_charge - min_charge) / bin_size;
-  Double_t charge_low = 0;
-  Double_t charge_up  = 0;
-
-  for (int i = 0; i < number_of_divisions; i++){
-
-    // Calculate the boundaries of the current bin.
-    charge_low = ( i       * bin_size ) + min_charge;
-    charge_up  = ( (i + 1) * bin_size ) + min_charge;
-    /*
-    WriteF90Hist(run, da_both_dir, charge_low, charge_up, f90_min, f90_max, Form("f90_histogram_%d", i+1),      Form("f90 Distribution (Charge Interval: %d - %d PE); f90", (int) charge_low, (int) charge_up) );
-
-    WriteF90Hist(run, da_er_dir,   charge_low, charge_up, f90_min, f90_max, Form("f90_histogram_er_%d", i+1),   Form("f90 Distribution (ER, Charge Interval: %d - %d PE); f90", (int) charge_low, (int) charge_up) );
-
-    WriteF90Hist(run, da_nr_dir,   charge_low, charge_up, f90_min, f90_max, Form("f90_histogram_nr_%d", i+1),   Form("f90 Distribution (NR, Charge Interval: %d - %d PE); f90", (int) charge_low, (int) charge_up));
-
-    WriteF90Hist(run, mc_er_dir,   charge_low, charge_up, f90_min, f90_max, Form("f90_histogram_mcer_%d", i+1), Form("f90 Distribution (MC ER, Charge Interval: %d - %d PE); f90", (int) charge_low, (int) charge_up))
-
-    WriteF90Hist(run, mc_nr_dir,   charge_low, charge_up, f90_min, f90_max, Form("f90_histogram_mcnr_%d", i+1), Form("f90 Distribution (MC NR, Charge Interval: %d - %d PE); f90", (int) charge_low, (int) charge_up))
-    */
+  switch(ERorNR){
+    case 0:
+        CreateF90ERHistograms( run, exp_cfg, isMC, tof_cut, number_of_bins, min_charge, max_charge );
+        break;
+    case 1:
+        CreateF90NRHistograms( run, exp_cfg, isMC, tof_cut, number_of_bins, min_charge, max_charge );
+        break;
+    case 10:
+        CreateF90ERHistograms( run, exp_cfg, isMC, tof_cut, number_of_bins, min_charge, max_charge );
+        CreateF90NRHistograms( run, exp_cfg, isMC, tof_cut, number_of_bins, min_charge, max_charge );
+        break;
+    default:
+        std::cout << "Invalid 'ERorNR' parameter value. Plese enter one of the following options: 0 (ER), 1 (NR) or 10 (ER & NR)." << std::endl;
   }
-
-  hist_file -> Close();
 }
